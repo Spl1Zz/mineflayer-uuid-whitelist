076a89b945ab6e8b61f993d238f2310ca900f0f1
SpliZz <120951381+Spl1Zz@users.noreply.github.com>
2025-05-10 17:46:02 +0200

    Added whitelist functionality for offline accounts (1.1.6)

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // made by SpliZz
 const fetch = require('node-fetch');
 const fs = require('fs');
+ const configfile = "./uuidconfig.json";
+  if (!fs.existsSync(configfile)) {
+    fs.writeFileSync(configfile, JSON.stringify([], null, 2));
+    if (config.logs) {
+      console.log("uuidconfig.json was created.");
+    }
+  }
+const config = './uuidconfig.json'
 
-module.exports = function(bot, config) {
+module.exports = function(bot) {
   const whitelistFile = "./whitelist.json";
   if (!fs.existsSync(whitelistFile)) {
     fs.writeFileSync(whitelistFile, JSON.stringify([], null, 2));
@@ -10,6 +18,7 @@ module.exports = function(bot, config) {
       console.log("whitelist.json was created.");
     }
   }
+ 
 
   async function whitelist(action, username) {
     if (!bot) {
@@ -20,13 +29,33 @@ module.exports = function(bot, config) {
     let uuid;
     let uuids = [];
 
+
+    //add
     if (action === "add" && username) {
       const player = bot.players[username];
       if (!player) {
-        console.error("Couldn't add that player. They're not online.");
-        return;
+        if (config.logs) {
+          console.log(`This player is currently offline. Offline-adding...`);
+        }
+        const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        if (res.ok) {
+          const data = await res.json();
+          uuid = data.id;
+          if (fs.existsSync(whitelistFile)) {
+        uuids = JSON.parse(fs.readFileSync(whitelistFile));
       }
-
+          uuids.push(uuid);
+          fs.writeFileSync(whitelistFile, JSON.stringify(uuids, null, 2));
+          if (config.logs) {
+            console.log(`Successfully added player ${username} with the UUID ${uuid}. Total whitelisted: ${uuids.length}.`);
+          }
+        } else {
+          if  (config.logs) {
+            console.log(`Player ${username} not found. You either entered a wrong username or the player is cracked`);
+          }
+        }
+      }
+if (player) {
       const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${player.username}`);
       if (!res.ok) {
         console.error("Error fetching UUID");
@@ -48,14 +77,41 @@ module.exports = function(bot, config) {
         }
       }
     }
+  }
 
+
+  //remove
     else if (action === "remove" && username) {
       const player = bot.players[username];
       if (!player) {
-        console.error("Couldn't remove that player. They're not online.");
-        return;
+        
+        if (config.logs) {
+          console.log(`This player is currently offline. Offline-removing...`);
+        }
+        const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+         if (res.ok) {
+          const data = await res.json();
+          uuid = data.id;
+          if (fs.existsSync(whitelistFile)) {
+        uuids = JSON.parse(fs.readFileSync(whitelistFile));
       }
 
+      if (uuids.includes(uuid)) {
+        const index = uuids.indexOf(uuid);
+        if (index > -1) {
+          uuids.splice(index, 1);
+          fs.writeFileSync(whitelistFile, JSON.stringify(uuids, null, 2));
+          if (config.logs) {
+            console.log(`Successfully removed player ${data.name} with the UUID ${uuid}. Total whitelisted: ${uuids.length}.`);
+          }
+        }}
+      } else {
+          if  (config.logs) {
+            console.log(`Player ${username} not found. You either entered a wrong username or the player is cracked`);
+          }
+        }
+    }
+if (player) {
       const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${player.username}`);
       if (!res.ok) {
         console.error("Error fetching UUID");
@@ -80,14 +136,34 @@ module.exports = function(bot, config) {
         }
       }
     }
+  }
+  
 
+  //check
     else if (action === "check" && username) {
       const player = bot.players[username];
       if (!player) {
-        console.error("Couldn't check that player. They're not online.");
-        return false;
+        const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        if (res.ok) {
+          const data = await res.json();
+          uuid = data.id;
+          if (fs.existsSync(whitelistFile)) {
+        uuids = JSON.parse(fs.readFileSync(whitelistFile));
       }
 
+      if (uuids.includes(uuid)) {
+        return true;
+      } else {
+        return false;
+      }
+      } else {
+        if  (config.logs) {
+           console.log("Couldn't check that player. Either they're cracked or some other error occurred.");
+      }
+        return false;
+      }
+      }
+      if (player) {
       const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${player.username}`);
       if (!res.ok) {
         console.error("Error fetching UUID");
@@ -107,7 +183,9 @@ module.exports = function(bot, config) {
         return false;
       }
     }
+  }
 
+   //error when bot is not initialized
     else {
       console.error("\x1b[31m There was a problem with your Bot. (Probably not initialized) \x1b[0m");
     }
